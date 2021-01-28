80e7aa39d7cce2e35dcef0a0da2651596cdabd8a
Bezsilny <julek200@gmail.com>
2021-01-28 17:06:40 +0100

    adjusted menu for mobile devices

--- react-builder/src/components/Logo/Logo.js
+++ react-builder/src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import pizzaImage from '../../assets/images/pizza.png';
 import styles from './Logo.module.css';
 
 const logo = (props) => (
-    <div className={styles.Logo}>
+    <div className={styles.Logo} style={{height: props.height}}>
         <img src={pizzaImage} alt='pizza'/>
     </div>
 )
