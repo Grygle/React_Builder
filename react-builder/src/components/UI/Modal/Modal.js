23a38e1f66561eaefe3239da02f111934ce6649a
Bezsilny <julek200@gmail.com>
2021-02-01 12:39:23 +0100

    improved performance by adding shouldComponentUpdate method

--- react-builder/src/components/UI/Modal/Modal.js
+++ react-builder/src/components/UI/Modal/Modal.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} backdropClicked={props.modalClosed}/>
-        <div 
-            className={styles.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+    //only update when modal is to be shown
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate(){
+        console.log('Modal - ComponentDidUpdate')
+    }
+
+    render() {
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} backdropClicked={this.props.modalClosed}/>
+                <div 
+                    className={this.styles.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
