f0722bb62bb6a42355281306ca20a744d020b5ca
Bezsilny <julek200@gmail.com>
2021-02-03 16:25:18 +0100

    Fixed Labels of ingredients to be in the same place vertically

--- react-builder/src/components/Product/Controls/Control/Control.js
+++ react-builder/src/components/Product/Controls/Control/Control.js
@@ -4,7 +4,7 @@ import styles from './Control.module.css';
 
 const control = (props) => (
     <div className={styles.Control}>
-        <div>{props.ingredientLabel}</div>
+        <div className={styles.Label}>{props.ingredientLabel}</div>
         <button onClick={props.removed} disabled={props.disabled}>Remove</button>
         <button onClick={props.added}>Add</button>
     </div>
