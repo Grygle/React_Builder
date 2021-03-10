7122a812d80c65c757318de7d496214426c526e2
Bezsilny <julek200@gmail.com>
2021-03-10 13:33:08 +0100

    Added CheckoutSummary component

--- react-builder/src/App.js
+++ react-builder/src/App.js
@@ -1,10 +1,12 @@
-import Layout from './containers/Layout/Layout'
-import Builder from './containers/Builder/Builder'
+import Layout from './containers/Layout/Layout';
+import Builder from './containers/Builder/Builder';
+import Checkout from './containers/Checkout/Checkout';
 function App() {
   return (
     <div>
       <Layout>
         <Builder />
+        <Checkout />
       </Layout> 
     </div>
   );
---
+++ react-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Product from '../../Product/Product';
+import Button from '../../UI/Button/Button';
+import styles from './CheckoutSummary.module.css';
+
+const CheckoutSummary = (props) => {
+    return(
+        <div className={styles.CheckoutSummary}>
+            <h1>Your pizza</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Product ingredients={props.ingredients}/>
+            </div>
+            <Button 
+                buttonType="Danger"
+                clicked>CANCEL</Button>
+            <Button 
+                buttonType="Success"
+                clicked>CONTINUE</Button>
+
+        </div>
+    );
+}
+
+export default CheckoutSummary;
