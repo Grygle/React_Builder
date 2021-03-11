2349d93df7b6b5f3a34f20df2e81cf8921d11639
Bezsilny <julek200@gmail.com>
2021-03-11 18:39:08 +0100

    Getting orders from the firebase and properly displaying them

--- react-builder/src/components/Order/Order.js
+++ react-builder/src/components/Order/Order.js
@@ -4,11 +4,38 @@ import React from 'react'
 
 import styles from './Order.module.css';
 
-const order = (props) => (
-    <div className={styles.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.60</strong></p>
-    </div>
-);
+const order = (props) => {
+    const ingredients = [];
+
+    //converting object to array of objects
+    for(let ingredientName in props.ingredients){
+        ingredients.push(
+            {
+                name: ingredientName, 
+                amount: props.ingredients[ingredientName]
+            }
+        )
+    }
+
+    const ingredientOutput = ingredients.map(ingredient => {
+        return <span 
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #cccccc',
+                padding: '5px',
+                boxShadow: '0 2px 2px #eeeeee'
+            }}
+            key={ingredient.name}>{ingredient.name} ({ingredient.amount})</span>
+    })
+
+    return (
+        <div className={styles.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
