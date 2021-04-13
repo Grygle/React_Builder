4fb00b63d12b1cfba3dbad926fb725c5d90cc072
Bezsilny <julek200@gmail.com>
2021-04-13 14:35:56 +0200

    adjusted Checkout, ContactData to use redux

--- react-builder/src/containers/Builder/Builder.js
+++ react-builder/src/containers/Builder/Builder.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Aux from '../../hoc/Auxiliary';
 import Product from '../../components/Product/Product';
 import Controls from '../../components/Product/Controls/Controls';
@@ -7,19 +9,10 @@ import OrderSummary from '../../components/Product/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import errorHandler from '../../hoc/errorHandler/errorHandler';
-
-const INGREDIENT_PRICE = {
-    salad: 0.5,
-    cheese: 0.5,
-    meat: 1.5,
-    bacon: 0.6
-}
+import * as actionTypes from '../../store/actions';
 
 class Builder extends Component {
     state = {
-        ingredients: null,
-        totalPrice: 2,
-        canPurchase: false,
         buying: false,
         loading: false,
         error: false
@@ -45,46 +38,7 @@ class Builder extends Component {
             .reduce((sum, element) => {
                 return sum + element;
             }, 0)
-        this.setState({canPurchase: sum > 0})
-    }
-
-    addIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount + 1;
-        //state should be updated in a immutable way
-        const updatedIngredients = {
-            ...this.state.ingredients
-        };
-        updatedIngredients[type] = updatedCount;
-        const priceAdd = INGREDIENT_PRICE[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice + priceAdd;
-
-        this.setState({
-            ingredients: updatedIngredients,
-            totalPrice: newPrice
-        })
-        this.updateCanPurchaseState(updatedIngredients);
-    }
-
-    removeIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        if(oldCount > 0){
-            const updatedCount = oldCount - 1;
-            const updatedIngredients = {
-                ...this.state.ingredients
-            };
-            updatedIngredients[type] = updatedCount;
-            const priceReduce = INGREDIENT_PRICE[type];
-            const oldPrice = this.state.totalPrice;
-            const newPrice = oldPrice - priceReduce;
-    
-            this.setState({
-                ingredients: updatedIngredients,
-                totalPrice: newPrice
-            })
-            this.updateCanPurchaseState(updatedIngredients);
-        }
+        return sum > 0;
     }
 
     buyHandler = () => {
@@ -120,21 +74,13 @@ class Builder extends Component {
         //     .catch(error => {
         //         this.setState({loading: false, buying: false});
         //     });
-        const queryParams = [];
-        for(let i in this.state.ingredients){
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
-        }
-        queryParams.push('price= ' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+
+        this.props.history.push('/checkout');
     }
 
     render(){
         const disabled = {
-            ...this.state.ingredients
+            ...this.props.ing
         }
         for (let key in disabled) {
             disabled[key] = disabled[key] <= 0;
@@ -143,23 +89,23 @@ class Builder extends Component {
 
         let product = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
 
-        if(this.state.ingredients !== null){
+        if(this.props.ing !== null){
             product = (
                 <Aux>
-                    <Product ingredients={this.state.ingredients}/>
+                    <Product ingredients={this.props.ing}/>
                     <Controls 
-                        ingredientAdded={this.addIngredientHandler}
-                        ingredientRemoved={this.removeIngredientHandler}
+                        ingredientAdded={this.props.onIngredientAdded}
+                        ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabled}
-                        canPurchase={this.state.canPurchase}
+                        canPurchase={this.updateCanPurchaseState(this.props.ing)}
                         ordered={this.buyHandler}
-                        price={this.state.totalPrice}
+                        price={this.props.price}
                     />
                 </Aux>
             )
             orderSummary =  <OrderSummary 
-                ingredients={this.state.ingredients}
-                price={this.state.totalPrice}
+                ingredients={this.props.ing}
+                price={this.props.price}
                 buyCancel={this.cancelBuyingHandler}
                 buyContinue={this.continueBuyingHandler}/>
         }
@@ -178,4 +124,18 @@ class Builder extends Component {
     }
 }
 
-export default errorHandler(Builder, axios);
+const mapStateToProps = state => {
+    return {
+        ing: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
+        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(errorHandler(Builder, axios));
